2d4384db1102a066abebf8fced3e8e7190a5e130
qwinq <qwinq@outlook.com>
2020-03-05 17:43:45 +0800

    01_区块头创建

--- main.go
+++ main.go
@@ -33,5 +33,5 @@ func main() {
 	block := NewBlock("first bitCoin", []byte{})
 	fmt.Printf("前区块哈希值:%x\n", block.PrevHash)
 	fmt.Printf("当前区块哈希值:%x\n", block.Hash)
-	fmt.Printf("数据:%s\n", string(block.Data)) //...
+	fmt.Printf("数据:%s\n", string(block.Data)) //right now
 }
